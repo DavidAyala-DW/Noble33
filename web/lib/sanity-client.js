f948619cd3daf2393c5e992d4d94114fc58a2d57
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-09-24 16:05:36 -0500

    Migrate CMS

--- web/lib/sanity-client.js
+++ web/lib/sanity-client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client'
 
 const client = sanityClient({
-  projectId: '65oyeak4',
+  projectId: 'kq13uftm',
   dataset: 'production',
   useCdn: false, // `false` if you want to ensure fresh data
   apiVersion: '2022-06-29',
