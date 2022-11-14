edda48bb09128fb9164450553d6dcd16c01ee757
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-11-13 23:45:53 -0500

    add all urls

--- cms/resolve-production-url.js
+++ cms/resolve-production-url.js
@@ -1,13 +1,21 @@
 // Any random string, must match SANITY_PREVIEW_SECRET in the Next.js .env.local file
 const previewSecret = "test"
 
+const urls = {
+  "casaMadera": "https://casa-madera.netlify.app/",
+  "sparrow": "https://noble33-sparrow.netlify.app/",
+  "noble33": "https://noble33.netlify.app/"
+}
+
 const remoteUrl = `https://noble33.netlify.app/`
 const localUrl = `http://localhost:3000`
 
 export default function resolveProductionUrl(doc) {
   
+  const site = doc?.site ?? "noble33"
+
   const baseUrl =
-    window.location.hostname === 'localhost' ? localUrl : remoteUrl
+    window.location.hostname === 'localhost' ? localUrl : urls[site]
 
   const previewUrl = new URL(baseUrl)
 
