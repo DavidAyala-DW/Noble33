1f47a43aca29ba6ffe5ee33d46ff304bd95de95e
Austen Morgan <austen@boundaryla.com>
2023-02-15 17:37:09 -0800

    Resize collectionGrid title and CTA

--- web/components/sections/collectionGrid.js
+++ web/components/sections/collectionGrid.js
@@ -8,24 +8,20 @@ export default function CollectionGrid(props) {
 
     <section className="px-4 md:px-0 md:max-w-[93.3%] w-full mx-auto flex flex-col">
 
-      <div className="flex flex-col space-y-2 vw:space-y-[.4166vw] mb-8 lg:mb-[22px] vw:mb-[1.1458vw]">
+      <div className="flex flex-col space-y-2 vw:space-y-[.4166vw] mb-8 lg:mb-7 vw:mb-[1.1458vw]">
 
-        <h2 className="uppercase tracking-[.05em] opacity-80 text-lg vw:text-[.9375vw] leading-[21px] vw:leading-[1.16] font-medium" >
+        <h2 className="pb-2 uppercase tracking-[.05em] opacity-80 text-2xl vw:text-[.9375vw] leading-[21px] vw:leading-[1.16] font-light" >
           {title}
         </h2>
 
-        {
-          view_all?.title && (
-
-            <a
+        {view_all?.title ? (
+          <a
             href={view_all.link}
-            className="block uppercase hover_state_link opacity-80 tracking-[.05em] text-xs vw:text-[.625vw] leading-[1.1] font-light"
-            >
-              {view_all.title}
-            </a>
-
-          )
-        }
+            className="block uppercase hover_state_link opacity-80 tracking-[.05em] text-lg vw:text-[.625vw] leading-[1.1] font-light"
+          >
+            {view_all.title}
+          </a>
+        ) : null}
 
       </div>
 
