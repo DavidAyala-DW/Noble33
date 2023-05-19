09fc890f31160820c55d5b5bd535f007542d7e48
Austen Morgan <austen@boundaryla.com>
2023-05-19 16:05:26 -0700

    Fix grammar in cookie notice

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,31 +1,27 @@
-import '../styles/globals.scss'
-import Link from "next/link";
-import CookieConsent from "react-cookie-consent";
-import GTM from "@/components/GTM";
-import Cookies from 'js-cookie'
+import '../styles/globals.scss';
+import Link from 'next/link';
+import CookieConsent from 'react-cookie-consent';
+import GTM from '@/components/GTM';
+import Cookies from 'js-cookie';
 import { useEffect } from 'react';
 import AccessiBe from '@/components/accessibe';
 import Head from 'next/head';
 
-
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
+    const allCookies = Cookies.get('_gtm_all_cookies');
 
-    const allCookies =Cookies.get("_gtm_all_cookies");
-    
-    if(allCookies && allCookies == true){
+    if (allCookies && allCookies == true) {
       const dataLayer = window.dataLayer;
-      if(typeof dataLayer !== 'undefined'){
-        dataLayer.push({event: "all-cookies"});
+      if (typeof dataLayer !== 'undefined') {
+        dataLayer.push({ event: 'all-cookies' });
       }
     }
-
   }, []);
 
   return (
     <>
-      <GTM/>
+      <GTM />
       <AccessiBe />
       <Component {...pageProps} />
       <Head>
@@ -46,32 +42,24 @@ function MyApp({ Component, pageProps }) {
         expires={150}
         onAccept={() => {
           const dataLayer = window.dataLayer;
-          if(typeof dataLayer !== 'undefined'){
-            dataLayer.push({event: "all-cookies"});
+          if (typeof dataLayer !== 'undefined') {
+            dataLayer.push({ event: 'all-cookies' });
           }
         }}
       >
-
-        We use cookies to ensure that we give you the best experience on our website.
-        visit
-        
+        We use cookies to ensure that we give you the best experience on our
+        website. Visit our{' '}
         <Link href="/privacy-policy" passHref>
-          <a className="px-2 underline">
-            Privacy policy
-          </a>            
-        </Link>
-        
-        and
-
+          <a className="underline">Privacy policy</a>
+        </Link>{' '}
+        and{' '}
         <Link href="/website-terms" passHref>
-          <a className="px-2 underline">
-            Website Terms
-          </a>          
+          <a className="underline">Website Terms</a>
         </Link>
-
+        .
       </CookieConsent>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
