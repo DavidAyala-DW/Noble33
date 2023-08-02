d6f72df82800c1e8147b70c65c75edd06273e772
Austen Morgan <austen@boundaryla.com>
2023-08-02 11:21:14 -0700

    Add validation to contact fields to avoid breaking app

--- cms/schemas/documents/contact.js
+++ cms/schemas/documents/contact.js
@@ -1,4 +1,4 @@
-import { EnvelopeIcon } from '@sanity/icons'
+import { EnvelopeIcon } from '@sanity/icons';
 
 export default {
   name: 'contact_fields',
@@ -7,94 +7,90 @@ export default {
   icon: EnvelopeIcon,
   fields: [
     {
-      name: "venue_name",
-      title: "Venue Name",
-      type: "string",
+      name: 'venue_name',
+      title: 'Venue Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: "website",
-      title: "Website",
-      type: "url",
+      name: 'website',
+      title: 'Website',
+      type: 'url',
     },
     {
-      name: "email",
-      title: "Email",
-      type: "string",
+      name: 'email',
+      title: 'Email',
+      type: 'string',
     },
     {
-      name: "phone_number",
-      title: "Phone number",
-      type: "string",
+      name: 'phone_number',
+      title: 'Phone number',
+      type: 'string',
     },
     {
-      type: "image",
-      title: "Image",
-      name: "image",
+      type: 'image',
+      title: 'Image',
+      name: 'image',
+      validation: (Rule) => Rule.required(),
     },
     {
       name: 'schedules',
-      title: "Schedules",
+      title: 'Schedules',
       type: 'array',
       of: [
         {
           type: 'object',
-          name: "schedule",
-          title: "Schedule",
-          fields:[
+          name: 'schedule',
+          title: 'Schedule',
+          fields: [
             {
-              name: "schedule_title",
-              title: "Schedule title",
-              type: "string"
+              name: 'schedule_title',
+              title: 'Schedule title',
+              type: 'string',
             },
             {
-              name: "schedule_options",
-              title: "Schedule options",
-              type: "array",
+              name: 'schedule_options',
+              title: 'Schedule options',
+              type: 'array',
               of: [
                 {
-                  type: "object",
-                  name: "schedule_option",
-                  title: "Schedule option",
+                  type: 'object',
+                  name: 'schedule_option',
+                  title: 'Schedule option',
                   fields: [
                     {
-                      type: "string",
-                      name: "days",
-                      title: "Days"
+                      type: 'string',
+                      name: 'days',
+                      title: 'Days',
                     },
                     {
-                      type: "string",
-                      name: "hours",
-                      title: "Hours"
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
+                      type: 'string',
+                      name: 'hours',
+                      title: 'Hours',
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
       ],
     },
     {
       name: 'address',
-      title: "Address",
-      type: "text",
+      title: 'Address',
+      type: 'text',
     },
     {
       name: 'about_us',
-      title: "About us (Summary)",
-      type: "text",
+      title: 'About us (Summary)',
+      type: 'text',
     },
   ],
   preview: {
     select: {
-      site: "venue_name",
-      media: "image"
-    },
-    prepare({ site, media }) {
-      return {
-        title: site,
-        media
-      }
+      title: 'venue_name',
+      media: 'image',
     },
   },
-}
+};
